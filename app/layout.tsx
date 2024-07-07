da52e67d0c589beb252263d21f5c6ff5fb7df309
qiaocc <qiaocc@qiaocc.com>
2024-07-08 00:24:23 +0800

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type {Metadata} from "next";
 import "./globals.css";
 import {Analytics} from "@vercel/analytics/react"
 import {SpeedInsights} from "@vercel/speed-insights/next"
+import Script from "next/script";
 
 export const metadata: Metadata = {
     title: "Fake Screenshot",
